4794f6ce2060754d8c2b64f6b121f064be2280dc
tempshyam <sg1673417@gmail.com>
2025-09-18 11:13:36 +0530

    Few Bug fix in code & add feature for call ambulance

--- healplus2.0/src/components/CallDocNAmbulance.jsx
+++ healplus2.0/src/components/CallDocNAmbulance.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
-import '../styles/CallDocNAmbulance.css'
+import React from "react";
+import "../styles/CallDocNAmbulance.css";
 
 export default function CallDocNAmbulance() {
+  // Change the phone number below to the emergency number you want to dial
+  const ambulanceNumber = "+919831834794";
+
   return (
-    <div className='CallDocNAmbulance'>
-        <button class="ambulance">
-          <div>📞</div>
-          <p>Call an Ambulance</p>
-        </button>
-        <button class="ambulance">
-          <div>🩺</div>
-          <p>Consult <br />Doctor</p>
-        </button>
+    <div className="CallDocNAmbulance">
+      {/* Use an anchor with tel: so mobile devices open the dialer. Add aria for accessibility. */}
+      <a
+        className="ambulance"
+        href={`tel:${ambulanceNumber}`}
+        aria-label={`Call ambulance at ${ambulanceNumber}`}
+        role="button"
+        rel="noopener noreferrer"
+      >
+        <div>📞</div>
+        <p>Call an Ambulance</p>
+      </a>
+
+      <button className="ambulance">
+        <div>🩺</div>
+        <p>
+          Consult <br />
+          Doctor
+        </p>
+      </button>
     </div>
-  )
+  );
 }
