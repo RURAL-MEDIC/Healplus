5b5f83b61c65b864bd708db7616d47999800086b
SwarupPal2006 <24f2000340@ds.study.iitm.ac.in>
2025-09-20 00:36:05 +0530

    Ai Chatbot added and its dependency also + API fetch

--- healplus2.0/src/pages/Medicine.jsx
+++ healplus2.0/src/pages/Medicine.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import "../styles/Medicine.css"
+import "../styles/medicine.css"
 
 const Medicine = () => {
     return (
         <div className='Medicine-container'>
             <div className='recently-view'>
                 <h2>Recently Viewed</h2>
-            <div class="card-contaier">
-            <div class="card">
+            <div className="card-contaier">
+            <div className="card">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyQnRWuXWLoWOz5l_msBpBwwB83ZlWoMFPfA&s" alt=""/>
-              <div class="card-content">
+              <div className="card-content">
                 <h3>Aspirin</h3>
                 <h5 id="price1">Price: ₹500</h5>
                 
