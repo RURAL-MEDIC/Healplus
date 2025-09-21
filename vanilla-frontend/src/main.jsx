fb23dd1d65405cfe853fbf0a1f6190754cede805
Priyabhunia <priyabhunia39@gmail.com>
2025-09-22 00:44:11 +0530

    video call feature

--- vanilla-frontend/src/main.jsx
+++ vanilla-frontend/src/main.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home.jsx'
 import ChatBotAI from './pages/aiinterface.jsx'
 import Medicine from './pages/Medicine.jsx'
 import Doctor from './pages/Doctor.jsx'
+import DocDashboard from './components/DocDashboard.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -16,7 +17,8 @@ createRoot(document.getElementById('root')).render(
           <Route index element={<Home/>}/>
           <Route path="/chat" element={<ChatBotAI />} />
           <Route path="/doctor" element={<Doctor />} />
-          <Route path="/medicine" element={<Medicine />} />
+          <Route path="/medicine" element={<Medicine />} /> 
+          <Route path="/doc-dashboard" element={<DocDashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
