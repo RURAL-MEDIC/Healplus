a7689c42a39278865a812af051351c0059f2577a
unknown <mukherjeedebojit@gmail.com>
2025-09-21 23:09:07 +0530

    Added Medicine Page

--- vanilla-frontend/src/pages/Medicine.jsx
+++ vanilla-frontend/src/pages/Medicine.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import "../styles/medicine.css"
+import CallShop from '../components/medicine/CallShop.jsx';
+import SuggestedMedicine from '../components/medicine/SuggestedMedicine';
+import RecentlyViewedMedicine from '../components/medicine/RecentlyViewedMedicine';
 
 const Medicine = () => {
     return (
-        <div className='Medicine-container'>
-            <div className='recently-view'>
+        <div className='Medicine'>
+            {/* <div className='recently-view'>
                 <h2>Recently Viewed</h2>
             <div className="card-contaier">
             <div className="card">
@@ -12,11 +15,15 @@ const Medicine = () => {
               <div className="card-content">
                 <h3>Aspirin</h3>
                 <h5 id="price1">Price: ₹500</h5>
-                
                </div>
             </div>
             </div>
-            </div>
+            </div> */}
+
+            {/* <EmergencyBtn/> */}
+            <CallShop/>
+            <SuggestedMedicine/>
+            <RecentlyViewedMedicine/>
         </div>  
     );
 }
